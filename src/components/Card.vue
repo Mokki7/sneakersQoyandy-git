c526681b44e64a46bdefd7b5ed299e7164ac8907
Moki <stwdamir100@gmail.com>
2024-05-18 17:08:38 +0500

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+
+defineProps({
+    id: Number,
+    title: String,
+    imageUrl: String,
+    price: Number,
+    isFavorite: Boolean,
+    isAdded: Boolean,
+    onClickFavorite: Function,
+    onClickAdd: Function
+})
+
+
+
+</script>
+
+<template>
+    <div
+        class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
+        <img v-if="onClickFavorite" :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'" alt="Like 1"
+            :class="{ 'border-slate-200': !isFavorite, 'border-red-300': isFavorite, 'border-4': true, 'rounded-full': true, 'absolute': true, 'top-8': true, 'left-8': true }"
+            @click="onClickFavorite">
+        <img :src="imageUrl" alt="Sneaker">
+        <p class="mt-2">{{ title }}</p>
+        <div class="flex justify-between mt-5">
+            <div class="flex flex-col">
+                <span class="text-slate-400">Цена:</span>
+                <b>{{ price }} KZT.</b>
+            </div>
+            <img v-if="onClickAdd" @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt=" Plus">
+        </div>
+    </div>
+</template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const emit = defineEmits(['onClickRemove']);
+
+defineProps({
+    id: Number,
+    title: String,
+    imageUrl: String,
+    price: Number,
+
+})
+
+</script>
+
+<template>
+
+    <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+        <img class="w-16 h-16" :src="imageUrl" :alt="title">
+        <div class="flex flex-col flex-1">
+            <p>{{ title }}</p>
+            <div class="flex justify-between mt-2 ">
+                <b class="flex-1">{{ price }} KZT.</b>
+                <img @click="emit('onClickRemove')" class="opacity-60 cursor-pointer hover:opacity-100 transition"
+                    src="/close.svg" alt="Close">
+            </div>
+        </div>
+    </div>
+
+
+</template>
